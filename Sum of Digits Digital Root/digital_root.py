03ea69ac86ca17e9090198a66ae8c7c73f493a8a
Uglykoyote <uglykoyote@yandex.ru>
2018-06-13 20:52:49 +0400

    Update digital_root.py

--- Sum of Digits Digital Root/digital_root.py
+++ Sum of Digits Digital Root/digital_root.py
@@ -3,13 +3,13 @@
 
 
 def digital_root(n):
-    if len(str(n)) == 1:
-        return n
-    else:
+    if len(str(n)) > 1:
         summ = 0
         for item in str(n):
             summ += int(item)
-        digital_root(summ)
+        return digital_root(summ)
+    elif len(str(n)) == 1:
+        return(n)
 
 
 print(digital_root(132189))
